24905e031df973248de598073a871320001abc19
Andres Elizalde <andep210399@gmail.com>
2025-03-10 17:11:05 +0200

    feat: :sparkles: lisfting state up

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import Post from "./Post";
 import styles from "./PostList.module.css";
 import NewPost from "./NewPost";
 
 export default function PostList() {
+    const [enteredBody, setEnteredBody] = useState('');
+    const [enteredAuthor, setEnteredAuthor] = useState('');
+
+    function bodyChangeHandler(event){
+        setEnteredBody(event.target.value)
+    }
+
+    function authorChangeHandler(event){
+        setEnteredAuthor(event.target.value)
+    }
+
   return (
     <>
-      <NewPost />
+      <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler}/>
       <ul className={styles.posts}>
-        <Post author="Andres" text="text example" />
+        <Post author={enteredAuthor} text={enteredBody} />
         <Post author="Camilo" text="text example" />
       </ul>
     </>
