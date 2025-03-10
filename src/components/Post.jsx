bffa8dee39f64f6df4b6b792468883f0a49191ea
Andres Elizalde <andep210399@gmail.com>
2025-03-10 12:44:27 +0200

    feat: :sparkles: Logic to select rando name from a list

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,12 @@
-export default function Post(){
-    return(
-        <div>
-            <p>Name Example</p>
-            <p>text example</p>
-        </div>
-    )
+const names = ["Maximilian", "Manuel", "Andres", "Camilo"];
+
+export default function Post() {
+  const chosenName =
+    names[Math.floor(Math.random() * (names.length - 1 - 0 + 1)) + 0];
+  return (
+    <div>
+      <p>{chosenName}</p>
+      <p>text example</p>
+    </div>
+  );
 }
