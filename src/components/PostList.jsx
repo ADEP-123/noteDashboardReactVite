827dfddd609a4f060ded4d83958972f0b3b63056
Andres Elizalde <andep210399@gmail.com>
2025-03-10 16:40:35 +0200

    feat: :sparkles: newPost form

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,11 +1,15 @@
 import Post from "./Post";
-import styles from "./PostList.module.css"
+import styles from "./PostList.module.css";
+import NewPost from "./NewPost";
 
 export default function PostList() {
   return (
-    <ul className={styles.posts}>
-      <Post author="Andres" text="text example" />
-      <Post author="Camilo" text="text example" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={styles.posts}>
+        <Post author="Andres" text="text example" />
+        <Post author="Camilo" text="text example" />
+      </ul>
+    </>
   );
 }
