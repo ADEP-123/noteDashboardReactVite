84dc5865f203ea3e1e34dc3a96e50f9f89354ceb
Andres Elizalde <andep210399@gmail.com>
2025-03-10 18:53:43 +0200

    feat: :sparkles: Rendering real posts not hardcoded

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,35 +1,36 @@
-import { useState } from "react";
 import Post from "./Post";
 import styles from "./PostList.module.css";
 import NewPost from "./NewPost";
 import Modal from "./Modal";
+import { useState } from "react";
 
 export default function PostList(props) {
-  const [enteredBody, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
+  const [posts, setposts] = useState([]);
 
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
+  function addPostHandler(postData) {
+    setposts(existingPosts => [postData, ...existingPosts]);
   }
 
   return (
     <>
       {props.isPosting ? (
         <Modal onClose={props.onStopPosting}>
-          <NewPost
-            onBodyChange={bodyChangeHandler}
-            onAuthorChange={authorChangeHandler}
-          />
+          <NewPost onCancel={props.onStopPosting} onAdd={addPostHandler} />
         </Modal>
       ) : null}
-      <ul className={styles.posts}>
-        <Post author={enteredAuthor} text={enteredBody} />
-        <Post author="Camilo" text="text example" />
-      </ul>
+      {posts.length != 0 ? (
+        <ul className={styles.posts}>
+          {posts.map(post => (
+            <Post key={post.body} author={post.author} text={post.body} />
+          ))}
+        </ul>
+      ) : null}
+      {posts.length === 0 ? (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There are no posts yet.</h2>
+          <p>Start adding some! </p>
+        </div>
+      ) : null}
     </>
   );
 }
